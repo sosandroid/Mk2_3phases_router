25b246f8c4930b8caa0c5a16820207f30f435038
sosandroid <ehavet@yahoo.fr>
2023-02-09 09:53:15 +0100

    Update unit_test.cpp
    rename function & macro

--- unit_test.cpp
+++ unit_test.cpp
@@ -5,23 +5,23 @@
 // Avantage : détecter une erreur de configuration
 // Inconvénient: difficilement maintenable car toutes les combinaisons doivent ajoutées à la main
 
-constexpr bool uint8_differs(uint8_t a, uint8_t b) {
+constexpr bool pin_differs(uint8_t a, uint8_t b) {
 	if (a == 0xff || b == 0xff || a != b) {
 		return true;
 	}
 	return false;
 }
-#define STATIC_ASSERT_UINT8T_DIFFERS(pin1, pin2) static_assert(uint8_differs(pin1, pin2), #pin1" == " #pin2);
+#define STATIC_ASSERT_PIN_DIFFERS(pin1, pin2) static_assert(pin_differs(pin1, pin2), #pin1" == " #pin2);
 
 namespace pindistribution_tests {
-	STATIC_ASSERT_UINT8T_DIFFERS(offPeakForcePin, tempSensorPin);
-	STATIC_ASSERT_UINT8T_DIFFERS(offPeakForcePin, watchDogPin);
-	STATIC_ASSERT_UINT8T_DIFFERS(offPeakForcePin, diversionPin);
-	STATIC_ASSERT_UINT8T_DIFFERS(offPeakForcePin, rotationPin);
-	STATIC_ASSERT_UINT8T_DIFFERS(offPeakForcePin, forcePin);
-	STATIC_ASSERT_UINT8T_DIFFERS(tempSensorPin, watchDogPin);
-	STATIC_ASSERT_UINT8T_DIFFERS(tempSensorPin, diversionPin);
-	STATIC_ASSERT_UINT8T_DIFFERS(tempSensorPin, rotationPin);
-	STATIC_ASSERT_UINT8T_DIFFERS(tempSensorPin, forcePin);
+	STATIC_ASSERT_PIN_DIFFERS(offPeakForcePin, tempSensorPin);
+	STATIC_ASSERT_PIN_DIFFERS(offPeakForcePin, watchDogPin);
+	STATIC_ASSERT_PIN_DIFFERS(offPeakForcePin, diversionPin);
+	STATIC_ASSERT_PIN_DIFFERS(offPeakForcePin, rotationPin);
+	STATIC_ASSERT_PIN_DIFFERS(offPeakForcePin, forcePin);
+	STATIC_ASSERT_PIN_DIFFERS(tempSensorPin, watchDogPin);
+	STATIC_ASSERT_PIN_DIFFERS(tempSensorPin, diversionPin);
+	STATIC_ASSERT_PIN_DIFFERS(tempSensorPin, rotationPin);
+	STATIC_ASSERT_PIN_DIFFERS(tempSensorPin, forcePin);
 	//[...]
 } //namespace pindistribution_tests
